b45478d0c0d02f12b63a60e77939b262a76b1060
pducolin <paola.ducolin@datadoghq.com>
2020-12-01 15:02:33 +0100

    add const space solution to day 1 part 2

--- src/day_01/part_2/code.py
+++ src/day_01/part_2/code.py
@@ -1,4 +1,4 @@
-def main():
+def solution_space_N():
   with open('input.txt') as f:
     total = 2020
     # O(N)
@@ -17,6 +17,29 @@ def main():
         b = numbers[j]
         complementars[total - a - b] = {'a': a, 'b': b }
 
+def solution_space_const():
+  with open('input.txt') as f:
+    total = 2020
+    # O(NlogN)
+    sorted_numbers = sorted([int(x) for x in f.read().split()])
+    # O(N^2)
+    for i in range(len(sorted_numbers)):
+      a = sorted_numbers[i]
+      left_index = i + 1
+      right_index = len(sorted_numbers) - 1
+      while left_index < right_index:
+        b = sorted_numbers[left_index]
+        c = sorted_numbers[right_index]
+        if a + b + c == total:
+          print('🎉 Result is {}'.format(a*b*c))
+          return
+
+        if a + b + c < total:
+          left_index += 1
+          continue
+
+        right_index -= 1
+
 
 if __name__ == "__main__":
-  main()
+  solution_space_const()
