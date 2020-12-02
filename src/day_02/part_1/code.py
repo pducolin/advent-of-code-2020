367a0342c088ed9eb8edfe5ca9fef0b333402f4d
pducolin <paola.ducolin@datadoghq.com>
2020-12-02 13:17:08 +0100

    Solution for day 2, part 1

---
+++ src/day_02/part_1/code.py
@@ -0,0 +1,15 @@
+def solution():
+  counter = 0
+  with open('input.txt') as f:
+    lines = [x for x in f.read().split('\n')] 
+    for line in lines:
+      rule, password = line.split(':')
+      min_max, letter = rule.split()
+      rule_min, rule_max = [int(x) for x in min_max.split('-')]
+      letter_in_password_count = password.count(letter)
+      if letter_in_password_count >= rule_min and letter_in_password_count <= rule_max:
+        counter += 1
+  print('🎉 Result is {}'.format(counter))
+
+if __name__ == "__main__":
+  solution()
