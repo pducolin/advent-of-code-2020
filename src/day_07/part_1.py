cd4b56beb77a50915da93decb907dc60e9fe0c9d
pducolin <paola.ducolin@datadoghq.com>
2020-12-07 13:39:07 +0100

    Add day 7 solution

---
+++ src/day_07/part_1.py
@@ -0,0 +1,42 @@
+import re
+
+
+def parse_rules(data):
+    rules = {}
+    raw_rules = [x for x in data.split('\n')]
+    pattern_parent = r'(\w* \w*) bags contain '
+    pattern_child = r'\d+ (\w* \w* )?(bags|bag)\.?'
+    for rule in raw_rules:
+        if 'no other bags' in rule:
+            continue
+        m = re.search(pattern_parent, rule)
+        if not m:
+            continue
+        parent_color = m.group(1)
+        children_start_index = len(m.group(0))
+        children = [x.strip() for x in rule[children_start_index:].split(',')]
+        for child in children:
+            m = re.search(pattern_child, child)
+            child_color = m.group(1).strip()
+            if child_color not in rules:
+                rules[child_color] = []
+            rules[child_color].append(parent_color)
+    return rules
+
+
+def solution(data):
+    rules = parse_rules(data)
+    to_check_colors = rules['shiny gold']
+    visited = set()
+    counter = 0
+    while len(to_check_colors) > 0:
+        to_check = to_check_colors.pop()
+        if to_check in visited:
+            continue
+        counter += 1
+        visited.add(to_check)
+        if to_check not in rules:
+            continue
+        to_check_colors.extend(rules[to_check])
+
+    return counter
---
+++ src/day_07/part_2.py
@@ -0,0 +1,39 @@
+import re
+
+
+def parse_rules(data):
+    rules = {}
+    raw_rules = [x for x in data.split('\n')]
+    pattern_parent = r'(\w* \w*) bags contain '
+    pattern_child = r'(\d+){1} (\w* \w*) (bags|bag)\.?'
+    for rule in raw_rules:
+        if 'no other bags' in rule:
+            continue
+        m = re.search(pattern_parent, rule)
+        if not m:
+            continue
+        parent_color = m.group(1)
+        children_start_index = len(m.group(0))
+        children = [x.strip() for x in rule[children_start_index:].split(',')]
+        for child in children:
+            m = re.search(pattern_child, child)
+            child_count = int(m.group(1).strip())
+            child_color = m.group(2).strip()
+            if parent_color not in rules:
+                rules[parent_color] = []
+            rules[parent_color].append({'color': child_color, 'count': child_count})
+    return rules
+
+
+def recurively_add_bags(rules, color):
+    counter = 0
+    inner_bags = rules.get(color, [])
+    for bag in inner_bags:
+        counter += bag['count'] + bag['count'] * recurively_add_bags(rules, bag['color'])
+    return counter
+
+
+def solution(data):
+    rules = parse_rules(data)
+    counter = recurively_add_bags(rules, 'shiny gold')
+    return counter
