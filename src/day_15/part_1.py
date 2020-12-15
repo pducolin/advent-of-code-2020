a1b6bbe1a6378104831110c441ea8386936e5736
pducolin <paola.ducolin@datadoghq.com>
2020-12-15 09:49:06 +0100

    add solution for day 15

---
+++ src/day_15/part_1.py
@@ -0,0 +1,21 @@
+def solution(data):
+    counters = {}
+    starting_numbers = [int(x) for x in data.split(',')]
+    for i in range(2020):
+        if len(starting_numbers) > 0:
+            current_number = starting_numbers.pop(0)
+            counters[current_number] = [i]
+            previous_number = current_number
+            continue
+        last_index = counters[previous_number][-1]
+        if len(counters[previous_number]) > 1:
+            first_index = counters[previous_number].pop(0)
+        else:
+            first_index = last_index
+        current_number = last_index - first_index
+        if current_number not in counters:
+            counters[current_number] = []
+        counters[current_number].append(i)
+        previous_number = current_number
+
+    return previous_number
